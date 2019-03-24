327f6acb67bbeb8a51fcfd9b950b0a7c4164419d
Alina <alinacherez@gmail.com>
2019-03-24 22:33:24 +0300

    added files

---
+++ data.js
@@ -0,0 +1,43 @@
+var text = { "ru" : {
+					"introduction" :
+						"Привет! 👋 <br/><br/> Добро пожаловать в <b>Chatex</b> - бот-сервис по покупке и продаже биткоина, криптовалют и токенов. Автоматический бот <b>Chatex</b> выступает третьей стороной, гарантирующей сделки между людьми. <br/><br/> Что дальше?",
+					"information" : [
+					    "Отлично! В автоматическом боте <b>Chatex</b> ты просто отправляешь сообщения через свой любимый мессенджер, чтобы быстро и безопасно купить, продать или обменять биткоин и другие криптовалюты на обычные (фиатные) деньги. Безопасность всех сделок обеспечена простой и надежной блокчейн системой и повышенным уровнем киберзащиты, а степень защиты <b>PIN</b>-кода сильнее банковских стандартов.",
+					    "<b>Chatex</b> регистрирует для вас мультисиг криптокошелек в доверенном депозитарии <b><u><a href='https://www.bitgo.com/info/' target='_blank'>BitGo</a></u></b> и обеспечивает управление им через мессенджер. Мгновенные внутренние переводы без комиссий, абсолютная мобильность и простота использования позволяют оперативно реагировать на управление вашими цифровыми активами с помощью смартфона. Спорные ситуации регулируются <b>Chatex</b> в режиме реального времени. В будущем вас ждет еще больше возможностей!"
+					    ],
+					"networks" : [
+						"В настоящее время разрабатывается программный интерфейс <b>Chatex</b> для <b>Viber</b>. Уже почти всё готово!💪",
+						"В настоящее время разрабатывается программный интерфейс <b>Chatex</b> для <b>Facebook Messenger</b>. Уже почти всё готово!💪"
+					],
+					"start" : "Давайте начнем!",
+					"input" : [
+						"Попробовать прямо сейчас!",
+						"Узнать больше",
+						"Попробовать!",
+						"Telegram",
+						"Viber (скоро)",
+						"Facebook (скоро)",
+						"Попробовать Telegram"
+					]
+
+				},
+			"en" : {
+					"introduction" :
+						"Hello there! 👋 <br><br> Welcome to <b>Chatex</b> – the cross-messenger bot-service for buying and selling bitcoin, cryptocurrency and tokens. The automated <b>Chatex</b> bot facilitates guaranteed transactions between private individuals. <br><br> How can I be of assistance?",
+					"information" : ["Great! With the <b>Chatex</b> bot, you just text through your favorite messenger to quickly and safely buy, sell or exchange bitcoin and other cryptocurrencies for regular (fiat) money.  The privacy and reliability of all transactions are secured by advanced private-banking grade anti-fraud and <b>PIN</b>-code technology.","<b>Chatex</b> registers users with a <b><u><a target='_blank' href='https://www.bitgo.com/info/'>BitGo</a></u></b> multisignature crypto wallet that transacting parties can manage within the messenger. Instant internal transfers with no fees, total mobility and ease of use allow users to quickly respond to the management needs of digital asset portfolios with your smartphone. Settle disputes in real time with built in arbitration. More features coming soon!"],
+					"networks" : [
+						"The <b>Chatex</b> API for <b>Viber</b> is now under development. We're almost there! 💪",
+						"The <b>Chatex</b> API for <b>Facebook Messenger</b> is now under development. We're almost there! 💪"
+					],
+					"start" : "Let's get started!",
+					"input" : [
+						"Trade now!",
+						"Learn more",
+						"Try it!",
+						"Telegram",
+						"Viber (soon)",
+						"Facebook (soon)",
+						"Try Telegram"
+					]
+				}
+		};
